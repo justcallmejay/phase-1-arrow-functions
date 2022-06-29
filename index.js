e05862b48d97163c1d62f3abca40ff407b1a0927
jay <jaypifiverr@gmail.com>
2022-06-29 18:16:09 -0400

    commit tree 999018ddd97aa5a7db7fa94e4360b9fd2553c08d

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (a, b) => a / b 
+
+console.log(divide(2000, 100))
+
+//////
+
+const square = x => x**2
+
+console.log(square(2))
+
+//////
+
+const add = (a, b) => a + b
+
+console.log(3, 4)
